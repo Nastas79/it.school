03335dfd44626775048ef1dc5c491b75a33d81b3
Nastas79 <krysti.nastas.79@gmail.com>
2025-02-20 19:41:59 +0200

    string

---
+++ exemplu_string = 'a'.py
@@ -0,0 +1,89 @@
+# exemplu_string = 'a'
+# exemplu_string2 = ''
+# print(exmeplu_string)
+
+# nr1 = 7
+# nr2 = 10
+
+# print(nr1 < nr2 and nr2 > 15)
+# print(nr1 < nr2 or nr2 > 15)
+
+# variabila = 'text'
+# print(type(variabila))
+
+# variabila = 2
+# print(type(variabila))
+
+# ceva = 2
+# print(type(ceva))
+# print(ceva)
+
+# ceva = str(ceva)
+# print (type(ceva))
+# print (ceva)
+
+# citat = 'Ana are "mere"'
+# print(citat)
+
+# poezie = "anisoara merge la padure/nsi culege niste mure"
+# print(poezie)
+
+# poezie = "anisoara merge la padure\nsi culege niste mure"
+# print(poezie)
+# poezie2 = "aniosara merge la padure\tsi culege niste mure "
+# print(poezie2)
+
+# var1 = 6
+# var2 = var1
+# print(var2)
+
+# var1 = 9
+# var2 = 3
+# var3 = var1 // var2
+# print(var3)
+
+
+# string1 = 'mama are mere'
+# string2 = 'Are'
+# # print(string1 + string2)
+# # print(string2 + string1)
+# # print(string2 += string1)
+
+# print(string2)
+
+# string3 = string1 * -1
+# print(string3)
+
+# nr1 = 5
+# nr2 = 7.5
+# string1 = "eu sunt numarul:"
+
+# print(string1 + str(nr2))
+# print(string1)
+
+# nr3 = 3.8
+# nr3_int = int(nr3)
+# print(nr3_int)
+# nr3_str = str(nr3)
+# print(nr3_str)
+
+
+# if 'a' in nr1:
+#     nr1 = float(nr1)
+# else:
+#     nr1 = int(nr1)
+
+# if 'b' in nr2:
+#     nr2 = float(nr2)
+# else:
+#     nr2 = int(nr2)
+
+# rezultat = int(nr1 + nr2)
+# print(nr1 + nr2)
+# print(int(nr1 + nr2))
+# print(float(rezultat))
+
+c = 8.7
+d = 6
+rez = c / d
+print(rez)
